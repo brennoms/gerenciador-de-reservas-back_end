60e684b98652ad1c95ec4c46be4074b35282b48e
brennoms <brennoms2004@gmail.com>
2025-04-25 22:33:22 -0300

    ajustes em nomes de variaveis

--- controllers/reservaController.js
+++ controllers/reservaController.js
@@ -1,11 +1,11 @@
 import { criarReservas, pegarReservas, excluirReservas } from '../models/reservaModel.js';
 
 export async function fazerReservas(req, res) {
-  const { user_id } = req.usuario;
+  const { usuario_id } = req.usuario;
   const { imovel_id } = req.params;
   const { reservas } = req.body;
   try {
-    await criarReservas(user_id, imovel_id, reservas);
+    await criarReservas(usuario_id, imovel_id, reservas);
     return res.json({ mensagem: 'reservas feita' });
   } catch (erro) {
     console.log(erro);
@@ -14,10 +14,10 @@ export async function fazerReservas(req, res) {
 }
 
 export async function buscarReservas(req, res) {
-  const { user_id } = req.usuario;
+  const { usuario_id } = req.usuario;
   const { imovel_id } = req.params;
   try {
-    const reservas = await pegarReservas(user_id, imovel_id);
+    const reservas = await pegarReservas(usuario_id, imovel_id);
     return res.json({ reservas });
   } catch (erro) {
     console.log(erro);
@@ -26,12 +26,12 @@ export async function buscarReservas(req, res) {
 }
 
 export async function deletarReservas(req, res) {
-  const { user_id } = req.usuario;
+  const { usuario_id } = req.usuario;
   const { imovel_id } = req.params;
   const { reservas } = req.body;
   const reservas_id = reservas.map(reserva => reserva._id);
   try {
-    await excluirReservas(user_id, imovel_id, reservas_id);
+    await excluirReservas(usuario_id, imovel_id, reservas_id);
     return res.json({ mensagem: 'reservas canceladas' });
   } catch (erro) {
     console.log(erro);
