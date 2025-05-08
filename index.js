787a5e0e1c20485b4e4332aa151e9d342e73ad74
brennoms <brennoms2004@gmail.com>
2025-05-08 18:25:17 -0300

    feat: inicia a rota calendario

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import { criarIndices } from './models/database.js';
 import reservaRoutes from './routes/reservaRoutes.js';
 import usuarioRoutes from './routes/usuarioRoutes.js';
 import imoveisRoutes from './routes/imoveisRoutes.js';
+import CalendarioRoutes from './routes/calendarioRoutes.js';
 
 criarIndices();
 
@@ -16,5 +17,6 @@ app.use(express.json());
 app.use('/api', usuarioRoutes);
 app.use('/api', imoveisRoutes);
 app.use('/api', reservaRoutes);
+app.use('/api', CalendarioRoutes);
 
 export default app;
--- routes/calendarioRoutes.js
+++ routes/calendarioRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express';
 
-import calendario from '../controllers/calendario.js';
+import calendarioRestrito from '../controllers/calendarioRestritoControler.js';
+import calendario from '../controllers/calendarioControler.js';
+import autenticarToken from '../middlewares/autenticarToken.js';
 
 const router = express.Router();
-router.get('/calendario', calendario);
+router.get('/calendario/restrito', autenticarToken, calendarioRestrito);
+router.get('/:user_id/calendario', calendario);
 
 export default router;
